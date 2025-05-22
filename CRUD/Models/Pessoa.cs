12fc04a76b26cce2f4757311d74ce894f7f58d2b
Longhin08 <jv.longhin@outlook.com>
2025-05-22 20:10:01 -0300

    Configuração do Obejeto Pessoa

---
+++ CRUD/Models/Pessoa.cs
@@ -0,0 +1,18 @@
+﻿namespace CRUD.Models
+{
+    public class Pessoa
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+
+        public string Instrumento { get; set; }
+
+        public Pessoa(Guid id, string nome, string instrumento)
+        {
+            Id = id;
+            Nome = nome;
+            Instrumento = instrumento;
+        }
+        
+    }
+}
--- CRUD/Rotas/PessoaRotas.cs
+++ CRUD/Rotas/PessoaRotas.cs
@@ -1,10 +1,26 @@
-﻿namespace CRUD.Rotas
+﻿using CRUD.Models;
+
+
+namespace CRUD.Rotas
 {
     public static class PessoaRotas
     {
-        public static void MapPessoaRotas (this WebApplication app)
+        public static List<Pessoa> Pessoas = new()
         {
-            app.MapGet("/hello-World", () => new { Nome = "João" });
+            new (id: Guid.NewGuid(), nome: "John Frusciante", instrumento: "Guitarra"  ),
+            new (id: Guid.NewGuid(), nome: "Anthony Kiedis", instrumento: "Voz"),
+            new (id: Guid.NewGuid(), nome: "Chad Smith", instrumento: "Bateria"),
+            new (id: Guid.NewGuid(), nome: "(Flea) Michael Peter", instrumento: "Baixo")
+
+        };
+        public static void MapPessoaRotas(this WebApplication app)
+        {
+            app.MapGet("/rhcp", () => Pessoas);
+
+            app.MapGet("/rhcp/{nome}", (string nome) =>
+            {
+                return Pessoas.Find(x => x.Nome == nome);
+            });
         }
     }
 }
